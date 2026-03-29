13c95ceaf06cdcac6d04c2bc7c947275cae2711d
Cxjzzi <94388054+Cxjzzi@users.noreply.github.com>
2026-03-28 22:58:44 -0400

    Change IR distance sensor pin and add flex sensor
    Updated pin assignments for IR distance sensor and added flex sensor definition.

--- config.h
+++ config.h
@@ -7,14 +7,18 @@
 #define STEP 9
 #define DIR 10
 
+
 // Motor steps per revolution (200 for 1.8-degree stepper)
 #define FULL_STEPS_PER_REV 200
 
 // Pin (analog) for IR distance sensor
-#define IR_DIST_READ A0
+#define IR_DIST_READ A3
 
 // Pin (analog) for current sensor
 #define CURRENT_READ A1
 
 // Pin for light sensor digital output
 #define LIGHT_READ 2
+
+//Pin for flex sensor 
+#define flex A0
